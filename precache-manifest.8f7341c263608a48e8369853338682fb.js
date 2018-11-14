3ba93abf7d094191cc91482116294c2b1583b379
randy <c88yang@edu.uwaterloo.ca>
2018-11-13 21:39:39 -0500

    Updates

---
+++ precache-manifest.8f7341c263608a48e8369853338682fb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96c71dbafb69400601629b8919bc738",
+    "url": "/mysite/static/media/selfie.e96c71db.jpg"
+  },
+  {
+    "revision": "71a65ebc55c79a070571",
+    "url": "/mysite/static/css/main.f957b4d0.chunk.css"
+  },
+  {
+    "revision": "693816632ac05e6ad03e",
+    "url": "/mysite/static/js/1.69381663.chunk.js"
+  },
+  {
+    "revision": "3ded49a2979a7fef5d5b",
+    "url": "/mysite/static/js/runtime~main.3ded49a2.js"
+  },
+  {
+    "revision": "f12bc981897259c84500c37cfcd5557d",
+    "url": "/mysite/static/media/resume2018.f12bc981.pdf"
+  },
+  {
+    "revision": "71a65ebc55c79a070571",
+    "url": "/mysite/static/js/main.71a65ebc.chunk.js"
+  },
+  {
+    "revision": "b7e9c6c86b51498f3fabac8a7c9d0002",
+    "url": "/mysite/static/media/sample.b7e9c6c8.png"
+  },
+  {
+    "revision": "0f23f8f4291411e8f9a79fcc7032b6ef",
+    "url": "/mysite/static/media/nonhier.0f23f8f4.png"
+  },
+  {
+    "revision": "d8b938a931a18399c4a3ee8006b38f30",
+    "url": "/mysite/static/media/footer-background.d8b938a9.jpg"
+  },
+  {
+    "revision": "40789519f2e79e5337b166b596c52ed6",
+    "url": "/mysite/static/media/linkedin-in-brands.40789519.svg"
+  },
+  {
+    "revision": "875d9a99034af8033afa44c995b09a45",
+    "url": "/mysite/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ycyrandy/mysite/precache-manifest.be664e129c49173b3d38753e19afa378.js"
+  "/mysite/precache-manifest.8f7341c263608a48e8369853338682fb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ycyrandy/mysite/index.html", {
+workbox.routing.registerNavigationRoute("/mysite/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
